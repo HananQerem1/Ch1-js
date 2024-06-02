d61738028255b4a7f6ce0a9cab9a6786cc844360
Hanan Qerem <hananqerem@gmail.com>
2024-06-02 21:52:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+(document.querySelector(".userProfile h2").textContent="Hello Hanan");
+console.log(document.querySelector(".userInfo p"));
+document.querySelector(".userInfo p").classList.add("style");
+console.log(document.querySelector(".userProfile h2"));
